a47ea9e46484b1ab9c2055f6790c075525ad8477
dragonnomada <dragonnomada123@gmail.com>
2023-05-15 13:09:47 -0600

    Initial Commit

---
+++ TodoStoreApp/TodoStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoStoreAppApp.swift
+//  TodoStoreApp
+//
+//  Created by Alan Badillo Salas on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
